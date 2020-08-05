ba6faff63f4fa8367b1b9d4a60306cf24a54ad6e
Carlos Lucas Brandão Silva <carlos.silva@minutrade.com>
2020-08-05 17:00:57 -0300

    Subida inicial do projeto.

---
+++ test/unit/logger.test.js
@@ -0,0 +1,22 @@
+const { assert } = require('chai');
+const sinon = require('sinon');
+
+describe('testes unitario logger.js', () => {
+  it('Registra chamada e testa se result.stream.write foi chamado', () => {
+    const loggerResult = require('../../lib/utils/logger');
+    const loggerWinstonSpy = sinon.spy(loggerResult, 'info');
+
+    loggerResult.stream.write('unit test');
+
+    assert.isNotNull(loggerResult);
+    assert.isTrue(loggerWinstonSpy.calledOnce, 'Deve ser chamado apenas uma vez');
+  });
+  it('Deve imprimir dados formatados para stdout (integer value)', () => {
+    const loggerResult = require('../../lib/utils/logger');
+    loggerResult.on('data', (data) => {
+      assert.strictEqual(data.level, 'info');
+      assert.strictEqual(data.message, 'unit test 123456');
+    });
+    loggerResult.info('unit test %s', 123456);
+  });  
+});
